1d84a488366582d97d89de853d4d2ec279e505b6
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-08-13 16:36:59 +0330

    add login errors handling

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,13 @@
-export const responseLogin = async(username , password) => {
+export const responseLogin = async (username, password) => {
     const response = await fetch("http://localhost:5074/api/auth/login", {
         method: "POST", headers: {
             "Content-Type": "application/json"
-        }, body: JSON.stringify({ username: username, password : password })
+        }, body: JSON.stringify({ username: username, password: password })
     });
 
     const data = await response.json();
-    return data
+    return {
+        status: response.status,
+        data,
+    };
 }
--- js/login.js
+++ js/login.js
@@ -3,15 +3,31 @@ import { responseLogin } from "../api/auth.js";
 const userUsername = document.getElementById("username");
 const userPassword = document.getElementById("password");
 const loginForm = document.getElementById("loginForm");
+const loginError = document.getElementById("loginError");
 
 
-loginForm.addEventListener("submit", async () => {
+loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const username = userUsername.value;
     const password = userPassword.value;
 
-    const res = await responseLogin(username,password);
-    console.log(res);
+    try {
+        const res = await responseLogin(username, password);
+        console.log(res);
+
+        if (res.status === 400) {
+            loginError.textContent = "Username or password is incorrect.";
+            loginError.classList.remove("hidden");
+            loginError.style.color = "red";
+            return;
+
+        }
+
+    } catch {
+        loginError.textContent = "Unable to connect to the server.";
+
+    }
+    
 
 });
 
