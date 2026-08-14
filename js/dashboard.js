392336f737ad3ec3ec97499aa5029cec15d61fdd
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-08-14 16:06:38 +0330

    add about and event listeners for pages

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,46 @@
 import "../input.css";
 import { fetchDashboard } from "../api/dashboard.js"
 
+const dashboardError = document.getElementById("dashboardError");
 
 const totalStudentsCount = document.getElementById("totalStudentsCount");
 const activeStudentsCount = document.getElementById("activeStudentsCount");
 const graduatedStudentsCount = document.getElementById("graduatedStudentsCount");
 const totalCoursesCount = document.getElementById("totalCoursesCount");
 
+const students = document.getElementById("students");
+const courses = document.getElementById("courses");
+const logout = document.getElementById("logout");
+
 
 const loadDashboard = async () => {
-    const data = await fetchDashboard()
+    try {
+        const data = await fetchDashboard()
 
-    totalStudentsCount.textContent = data.data.totalStudents;
-    activeStudentsCount.textContent = data.data.activeStudents;
-    graduatedStudentsCount.textContent = data.data.graduatedStudents;
-    totalCoursesCount.textContent = data.data.totalCourses;
-
+        totalStudentsCount.textContent = data.data.totalStudents;
+        activeStudentsCount.textContent = data.data.activeStudents;
+        graduatedStudentsCount.textContent = data.data.graduatedStudents;
+        totalCoursesCount.textContent = data.data.totalCourses;
+    } catch {
+        dashboardError.textContent = "Unable to connect to the server.";
+    }
 
 
 
 }
- await loadDashboard()
+await loadDashboard();
+
+
+logout.addEventListener("click" , () =>{
+    window.location.href = "./login.html";
+});
+
+
+students.addEventListener("click" , () =>{
+    window.location.href = "./students.html";
+});
+
+courses.addEventListener("click" , () =>{
+    window.location.href = "./courses.html";
+});
 
