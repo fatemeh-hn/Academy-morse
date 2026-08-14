a9b417204a898eee5e5352b73399992b3616f999
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-08-14 15:45:29 +0330

    fetch and display dashboard data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,24 @@
 import "../input.css";
 import { fetchDashboard } from "../api/dashboard.js"
 
-console.log("DASHBOARD JS IS RUNNING");
+
+const totalStudentsCount = document.getElementById("totalStudentsCount");
+const activeStudentsCount = document.getElementById("activeStudentsCount");
+const graduatedStudentsCount = document.getElementById("graduatedStudentsCount");
+const totalCoursesCount = document.getElementById("totalCoursesCount");
+
 
 const loadDashboard = async () => {
     const data = await fetchDashboard()
-    return data
+
+    totalStudentsCount.textContent = data.data.totalStudents;
+    activeStudentsCount.textContent = data.data.activeStudents;
+    graduatedStudentsCount.textContent = data.data.graduatedStudents;
+    totalCoursesCount.textContent = data.data.totalCourses;
+
+
+
 
 }
+ await loadDashboard()
+
