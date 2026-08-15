188d474ef0d29470b991d993b7249c658f198b85
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-08-16 01:28:06 +0330

    create reusable sidebar component

--- js/navigation.js
+++ js/navigation.js
@@ -1,20 +1,29 @@
+const sidebar = document.getElementById("sidebar");
+
+const response = await fetch("../components/sidebar.html");
+
+const html = await response.text();
+
+sidebar.innerHTML = html;
+
+
 const students = document.getElementById("students");
 const courses = document.getElementById("courses");
 const about = document.getElementById("about");
 const logout = document.getElementById("logout");
 
 students?.addEventListener("click", () => {
-  window.location.href = "./students.html";
+    window.location.href = "./students.html";
 });
 
 courses?.addEventListener("click", () => {
-  window.location.href = "./courses.html";
+    window.location.href = "./courses.html";
 });
 
 about?.addEventListener("click", () => {
-  window.location.href = "./about.html";
+    window.location.href = "./about.html";
 });
 
 logout?.addEventListener("click", () => {
-  window.location.href = "./login.html";
+    window.location.href = "./login.html";
 });
