07ee531c9487ff09918b292b6c439fc65ca999c4
Fatemeh <fatemeh.hosseini838383@gmail.com>
2026-08-16 20:07:51 +0330

    format API current time for display

--- js/about.js
+++ js/about.js
@@ -8,6 +8,18 @@ const version = document.getElementById("version");
 const author = document.getElementById("author");
 const currentTime = document.getElementById("currentTime");
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date);
+};
+
 const loadAbout = async () => {
     try{
         const res = await aboutApi();
@@ -15,7 +27,7 @@ const loadAbout = async () => {
         applicationName.textContent = res.data.applicationName;
         version.textContent = res.data.version;
         author.textContent = res.data.author;
-        currentTime.textContent = res.data.currentTime;
+        currentTime.textContent = formatDate(res.data.currentTime);
 
 
 
